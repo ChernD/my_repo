d70b92fa3ff75e4133fab100c5e7b81a4bcbf4b2
ChernD <60579601+ChernD@users.noreply.github.com>
2020-03-16 20:40:37 +0300

    vers1

--- dsp/loss_func.py
+++ dsp/loss_func.py
@@ -2,8 +2,10 @@ from scipy.signal import remez, freqz
 import  numpy as np
 
 
-def fir_remez_loss(N, array_sovp, fs):
+def fir_remez_loss(N, bstop, bpass, per_verh, per_niz,  fs):
     # array_sovp - точки совпадения АЧХ
-    b1 = remez(N, array_sovp, [1, 0], Hz=fs)
-    loss =
-    return
+    b1 = remez(N, [bpass, per_verh, per_niz, bstop], [1, 0], Hz=fs)
+    H1, m = freqz(b1, 1)
+    H0 = np.append(np.ones(bpass), )
+    loss = np.sum((H1 - H0)**2)/len(H1)
+    return loss
